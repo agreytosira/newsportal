dc36d08d3588526c1abdeccca4569463156f3880
Agrey Tosira <agreytosira@gmail.com>
2024-04-07 00:16:24 +0700

    fix: typo in news migrate

--- database/migrations/2024_04_06_170437_create_news_table.php
+++ database/migrations/2024_04_06_170437_create_news_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('news', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('descrption');
+            $table->string('description');
             $table->string('category');
             $table->string('author');
             $table->timestamps();
