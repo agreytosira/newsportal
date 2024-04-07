1f029d9c287af5a8a52b8494c8d12e337858cf3e
Agrey Tosira <agreytosira@gmail.com>
2024-04-07 17:28:25 +0700

    feat: render news data in homepage

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,13 +1,31 @@
 import { Head } from "@inertiajs/react";
 
-export default function Homepage({ title, description }) {
+export default function Homepage(props) {
+    console.log(props);
+
     return (
         <>
-            <Head title={title} />
+            <Head title={props.title} />
             <main>
-                <div className="container px-4">
-                    <h1 className="text-2xl font-bold">{title}</h1>
-                    <p>{description}</p>
+                <div className="container">
+                    <div className="py-4">
+                        <h1 className="text-2xl font-bold">{props.title}</h1>
+                        <p>{props.description}</p>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        {props.news.map((news) => {
+                            return (
+                                <div className="p-4 border border-slate-300">
+                                    <h1 className="text-xl font-bold">
+                                        {news.title}
+                                    </h1>
+                                    <p className="text-slate-600">
+                                        {news.description}
+                                    </p>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </main>
         </>
