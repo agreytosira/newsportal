9647e00c5b2410dc308777f65615562da4ec8369
Agrey Tosira <agreytosira@gmail.com>
2024-04-15 13:34:59 +0700

    feat: add logout function, add form in dasboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,18 +1,37 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 
 export default function Dashboard({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                    Dashboard
+                </h2>
+            }
         >
             <Head title="Dashboard" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="flex flex-col w-1/3 p-6 mx-auto space-y-4 bg-white border-b border-gray-200">
+                        <input
+                            type="text"
+                            placeholder="Judul"
+                            className="w-full input input-bordered"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Deskripsi"
+                            className="w-full input input-bordered"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Kategori"
+                            className="w-full input input-bordered"
+                        />
+                        <button className="btn btn-primary">SUBMIT</button>
                     </div>
                 </div>
             </div>
